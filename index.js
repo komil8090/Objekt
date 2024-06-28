e420779f4efc23e9a241911d6c30c815748176a5
Mirkomil Mirvohidov <mirkomilmirvohidov4@gmail.com>
2024-06-28 18:37:08 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let color = prompt("text color kiriting");
+let bgcolor = prompt("beckground-color kiriting");
+
+let body = document.querySelector("body");
+
+body.style = `color: ${color}; background-color: ${bgcolor}`
